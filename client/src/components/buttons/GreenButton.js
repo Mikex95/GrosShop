d5af229161f356b65deca5f89d3a50034a6da7f9
ElviraFrolow email =frolow.affiliate@gmail.com <supercoder@SuperCoders-Air.fritz.box>
2023-02-10 13:26:24 +0100

    add signup function and styling

--- client/src/components/buttons/GreenButton.js
+++ client/src/components/buttons/GreenButton.js
@@ -1,11 +1,11 @@
 import { Link } from "react-router-dom";
 import "./GreenButton.css";
 const GreenButton = ({ text }) => {
-	return (
-		<div className="green-button">
-			<Link className="green-button-link">{text}</Link>
-		</div>
-	);
+  return (
+    <div className="green-button">
+      <Link className="green-button-link">{text}</Link>
+    </div>
+  );
 };
 
 export default GreenButton;
--- client/src/pages/Verification/SignIn.js
+++ client/src/pages/Verification/SignIn.js
@@ -1,20 +1,71 @@
-const SignIn = () => {
+import GreenButton from "../../components/buttons/GreenButton";
+import { Link } from "react-router-dom";
+import { ReactComponent as Image } from "../../img/Logo-Login.svg";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { apiBaseUrl } from "../../api";
+
+import "./Verification.css";
+
+const SignIn = ({ setToken }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const navigate = useNavigate();
+
+  function login(event) {
+    event.preventDefault();
+
+    fetch(`${apiBaseUrl}/users/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+      credentials: "include",
+    })
+      .then((res) => res.json())
+      .then(({ status, result, error }) => {
+        if (status === "error") {
+          setErrorMessage(error.message);
+          return;
+        }
+        setToken(result.accessToken);
+        return navigate("/dashboard");
+      });
+  }
+
   return (
-    <div>
-      <div>
-        <div>
-          <img src="" alt="" />
+    <div className="verification">
+      <div className="verification-upper">
+        <div className="verification-image">
+          <Image />
         </div>
-        <form>
-          <input type="email" placeholder="Type you E-Mail" />
-          <input type="password" placeholder="Choose a password" />
-          <a href="">Forgot Password?</a>
-          <p>Button placeholder</p>
+        <form className="verification-form">
+          <label htmlFor="Email">Email</label>
+          <input
+            type="email"
+            placeholder="Type your E-Mail"
+            name="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <label htmlFor="Email">Password</label>
+          <input
+            type="password"
+            placeholder="Type your password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <GreenButton text="Sign In" onClick={login} />
+          {errorMessage && <p className="error-message">{errorMessage}</p>}
         </form>
       </div>
-      <div>
-        <p>Dont have an Account?</p>
-        <span>Sign up</span>
+      <div className="verification-to-signin">
+        <p>Don't have an Account?</p>
+        <Link to="/signup">Sign Up</Link>
       </div>
     </div>
   );
